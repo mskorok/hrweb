3ea66240684ae4d0e8d21fc0158da63e384c0138
mike <mskorok@gmail.com>
2022-03-21 16:00:05 +0300

    fixes

--- app/hr/controllers/educationsController.js
+++ app/hr/controllers/educationsController.js
@@ -12,6 +12,7 @@ angular.module('Educations', [], function () {
         function ($scope, $state, $cookies, $http, $templateCache, $location) {
 
             var page = $location.search().page;
+            $scope.show_select = true;
 
             if (!page) {
                 page = 1;
@@ -20,6 +21,7 @@ angular.module('Educations', [], function () {
             $scope.hr_rest_limit = 100;
             $scope.header_content = 'hr/templates/partial/header-content.html';
             $scope.header_background = 'hr/templates/partial/header-background.html';
+            $scope.header_search = 'hr/templates/partial/header-search-educations.html';
             $scope.top_menu = 'hr/templates/partial/top-menu.html';
             $scope.top_menu_mobile = 'hr/templates/partial/top-menu-mobile.html';
             $scope.footer = 'hr/templates/partial/footer.html';
@@ -38,6 +40,25 @@ angular.module('Educations', [], function () {
                         if (data.data.data.institutions) {
                             $scope.institutions = data.data.data.institutions
                         }
+                    });
+
+                    var select = jQuery("#banner");
+                    var options = select[0].querySelectorAll('option');
+
+                    $scope.show_select = true;
+                    select.chosen({width: "100px",});
+                    select.chosen().change(function () {
+                        $scope.lang = parseInt($(this).val());
+                        console.warn(2, $scope.lang);
+                        [].forEach.call(options, function (option) {
+                            option.removeAttribute('selected');
+                        });
+                        [].forEach.call(options, function (option) {
+                            var val = parseInt(option.getAttribute('value'));
+                            if (val === $scope.lang) {
+                                option.setAttribute('selected', 'selected');
+                            }
+                        });
                     });
                 }
             });
